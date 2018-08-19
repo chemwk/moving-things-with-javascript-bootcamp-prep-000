c751c5294942ece59b244116c22119c6fa345e7b
min kim <minwookkim2013@u.northwestern.edu>
2018-08-19 16:17:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = "I don't do much."
+
 function moveDodgerRight(){
     var leftNumbers = dodger.style.left.replace('px','')
     var left = parseInt(leftNumbers,10)
@@ -6,7 +7,7 @@ function moveDodgerRight(){
         dodger.style.left=`${left+1}px`
     }}
 
-  document.addEventListener("keydown",function(e){
+document.addEventListener("keydown",function(e){
         if(e.which===39){
             moveDodgerRight()
         }})
